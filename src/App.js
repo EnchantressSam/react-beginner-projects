2a80149e756121bc370e004a98f0b54610394ba7
Tatiana Loginova <tatyanka8samara@mail.ru>
2023-03-02 22:53:58 +0400

    component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './index.scss'
 
-function App() {
-  /*блок всегда есть, просто невидим */
+/*разметка как одельный компонент, получает пропсы open, setOpen */
+const Modal = ({ open, setOpen }) => (
+  <div className={`overlay animated ${open ? 'show' : ''}`}>
+    <div className="modal">
+      <svg
+        onClick={() => setOpen(false)}
+        height="200"
+        viewBox="0 0 200 200"
+        width="200"
+      >
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+    </div>
+  </div>
+)
 
+function App() {
   const [open, setOpen] = React.useState(false)
 
   return (
@@ -11,21 +27,10 @@ function App() {
       <button onClick={() => setOpen(true)} className="open-modal-btn">
         ✨ Открыть окно
       </button>
-
-      <div className={`overlay animated ${open ? 'show' : ''}`}>
-        <div className="modal">
-          <svg
-            onClick={() => setOpen(false)}
-            height="200"
-            viewBox="0 0 200 200"
-            width="200"
-          >
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div>
+      {/* можно компонент через условный рендер прописать 
+      {open && <Modal open={open} setOpen={setOpen} />}
+      */}
+      {<Modal open={open} setOpen={setOpen} />}
     </div>
   )
 }
