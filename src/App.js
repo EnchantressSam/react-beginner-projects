6475bf9722005c130de23e29cad2b508a7b5f05b
Tatiana Loginova <tatyanka8samara@mail.ru>
2023-03-02 16:45:53 +0400

    start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './index.scss';
+import './index.scss'
 
 function App() {
+  let count = 0
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
+        <h1>{count}</h1>
         <button className="minus">- Минус</button>
         <button className="plus">Плюс +</button>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
